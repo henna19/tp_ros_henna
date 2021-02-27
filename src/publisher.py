0efb127fe6782435548b42ac55e9ff45a5f57bcd
henna19 <hennarahman4@outlook.com>
2021-02-27 08:41:51 +0400

    Final

--- src/publisher.py
+++ src/publisher.py
@@ -4,42 +4,48 @@ import rospy
 import math
 from Tkinter import *
 from std_msgs.msg import Bool
+from std_msgs.msg import String
 from geometry_msgs.msg import PoseStamped
-import time 
 
 
-def callback(data):
+class huit:
+    def __init__(self):
+	rospy.init_node('talker', anonymous=True)
+	self.pub = rospy.Publisher('chatter', PoseStamped, queue_size=10)    
+	self.rate = rospy.Rate(15) # 15hz
+	self.message=PoseStamped()
+	self.theta=0
+	self.state=None
+	self.message.pose.position.x=self.theta
+	self.message.pose.position.y=math.sin(self.theta)
+	self.message.header.frame_id="map"
 
-    pub = rospy.Publisher('chatter', PoseStamped, queue_size=10)    
-    rate = rospy.Rate(15) # 15hz
-    message=PoseStamped()
-    theta=0
-    message.header.frame_id="map"
-    print(message)
+	rospy.Subscriber("button_state", Bool, self.callback)
 
+    def callback(self,data):
+	self.state = data.data
 
-    while(data.data == True):
+    def run(self):
 
-	while theta <= 2*(math.pi):
-		message.pose.position.x=theta
-		message.pose.position.y=math.sin(theta)
-		pub.publish(message)
-		rate.sleep()
-		theta = theta + 0.1
-
-	while theta >=0:
-		message.pose.position.x=theta
-		message.pose.position.y=math.sin(-theta)
-		pub.publish(message)
-		rate.sleep()
-		theta = theta - 0.1
-        rospy.Subscriber("button_state", Bool, callback)
-
+	while not rospy.is_shutdown():
+	    if(self.state == True):
+		while (self.theta <= 2*(math.pi)):
+			self.message.pose.position.x=(self.theta)
+			self.message.pose.position.y=math.sin(self.theta)
+			self.pub.publish(self.message)
+			self.rate.sleep()
+			self.theta = self.theta + 0.1
+		while (self.theta >=0):
+			self.message.pose.position.x=self.theta
+			self.message.pose.position.y=math.sin(-self.theta)
+			self.pub.publish(self.message)
+			self.rate.sleep()
+			self.theta = self.theta - 0.1
   
-def listener():
-    rospy.init_node('talker', anonymous=True)        
-    rospy.Subscriber("button_state", Bool, callback)
-    rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+    try:
+	node=huit()
+	node.run()
+    except rospy.ROSInterruptException:
+	pass
